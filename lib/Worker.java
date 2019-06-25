afb015699be8568c4675ab872e0babf75a699929
MG-Z <799595012@qq.com>
2019-06-25 14:47:33 +0800

    Update Worker.java

--- lib/Worker.java
+++ lib/Worker.java
@@ -1,12 +1,8 @@
-package com.archly.delivery.engine.worker;
+
 
 import java.util.concurrent.Future;
 
-/**
- * @author :zj.zou
- * @desc :
- * @date :2019.03.14
- */
+
 public interface Worker {
      boolean execute(Work work);
      Future<CallableWork.Result> submitCallable(CallableWork work);
